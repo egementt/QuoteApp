8baa69635ac83992068f03fdae3f6d9700ce8f1c
egementt <tokgozdev@gmail.com>
2021-08-29 01:47:46 +0300

    init

---
+++ app/src/main/java/com/example/currencyapi/models/QuoteDBModel.kt
@@ -0,0 +1,17 @@
+package com.example.currencyapi.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "quote")
+data class QuoteDBModel(
+    @PrimaryKey
+    @SerializedName("id")
+    var quoteId: Long,
+    var languageCode: String?,
+    var content: String,
+    @SerializedName("url")
+    var quoteUrl: String?,
+    var author: String
+)
